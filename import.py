f240d0fe0c7e6ba587f6a4ea658d5fde2e08771e
Connor J Thompson <ct770a@gmail.com>
2024-02-02 13:24:57 +1100

    fixed a buncha the structs, added bone support, added map support & added options to select which type of mesh you're importing

--- import.py
+++ import.py
@@ -5,13 +5,12 @@ import math
 
 
 model_header_size = 24
+class model_header_signature:
+    signature = ""
+    unk = 0
 class model_header:
-    unk1 = 0                # 0x00, 4bytes
-    signature = ""          # 0x04, 8bytes
-    unk2 = 0                # 0x0C, 2bytes
-    unk3 = 0                # 0x0E, 2bytes
-    something = 0           # 0x10, 4bytes
-    vert_byte_length = 0    # 0x14, 4bytes
+    sig_count = 0             
+    signatures = []
 
 model_vertex_size = 24
 class model_vertex:
@@ -36,16 +35,18 @@ class model_mesh_part:
     last_vert_index = 0 
     indices_offset = 0 
     triangles_count = 0
-    coord_stuff = []
-    _first_vert_index = 0
-    _indices_offset = 0
-    vert_count = 0
+    coord_stuff = [] # 32 bytes
+
 class model_mesh:
     name = "" # 0x00, 66bytes
     unk_count = 0
     unkers = []
     part_count = 0
     parts = []
+    coord_stuff = [] # 32 bnytes
+    first_vert_index = 0
+    indices_offset = 0
+    vert_count = 0 
 
 def read_model_mesh(f):
     f.read(4) # this int is unknown for now
@@ -71,12 +72,13 @@ def read_model_mesh(f):
             part.last_vert_index = read_uint(f)
             part.indices_offset = read_uint(f)
             part.triangles_count = read_uint(f)
-            part.coord_stuff = f.read(64)
-            part._first_vert_index = read_uint(f)
-            part._indices_offset = read_uint(f)
-            part.vert_count = read_uint(f)
+            part.coord_stuff = f.read(32)
             mesh.parts.append(part)
 
+        mesh.coord_stuff = f.read(32)
+        mesh.first_vert_index = read_uint(f)
+        mesh.indices_offset = read_uint(f)
+        mesh.vert_count = read_uint(f)
         meshes.append(mesh)
     return meshes
 
@@ -126,10 +128,12 @@ def read_fixed_string(f, length):
 
 def read_model_header(f):
     header = model_header()
-    header.unk1 = read_int(f)
-    header.signature = read_string(f)
-    header.unk2 = read_short(f)
-    header.unk3 = read_short(f)
+    header.sig_count = read_int(f)
+    for i in range(0, header.sig_count):
+        sig = model_header_signature()
+        sig.signature = read_string(f)
+        sig.unk = read_uint(f)
+        header.signatures.append(sig)
     return header
 
 
@@ -148,7 +152,114 @@ def read_model_indices(f):
     else: raise Exception("bad vert indices byte width")
     return indices
 
-def read_some_data(context, filepath, use_some_setting):
+def construct_meshes(meshes, verts, indices, bone_count = -1, bone_names = [], bone_orientations = [], bone_parents = []):
+    for mesh in meshes:
+        for part in mesh.parts:
+            #part_mat = bpy.data.materials.new(name=part.label)
+            #obj.data.materials.append(part_mat)
+            bpy_mesh = bpy.data.meshes.new("myMesh")
+            obj = bpy.data.objects.new(mesh.name + "_" + part.label, bpy_mesh)
+            bpy.context.collection.objects.link(obj)
+
+            # gather local verts
+            blender_verts = []
+            blender_UVs = []
+            blender_normals = []
+            for i in range(part.first_vert_index, part.last_vert_index+1):
+                vert = verts[i]
+                blender_verts.append((vert.x, vert.y, vert.z))
+                blender_UVs.append((vert.uv_x, vert.uv_y))
+                blender_normals.append((math.cos(vert.n_y)*math.cos(vert.n_x), math.sin(vert.n_y)*math.cos(vert.n_x), math.sin(vert.n_x))) 
+
+            # gather all the indices together
+            blender_indices = []
+            for i in range(0, part.triangles_count):
+                index = (i*3)+part.indices_offset
+                blender_indices.append((indices[index]-part.first_vert_index, indices[index+1]-part.first_vert_index, indices[index+2]-part.first_vert_index))
+
+            bpy_mesh.from_pydata(blender_verts, [], blender_indices)
+            
+            #########  EXTRA JUNK ########
+            # no idea what this does, bing gave me this
+            uv_layer = bpy_mesh.uv_layers.new()
+            bpy_mesh.uv_layers.active = uv_layer
+            for face in bpy_mesh.polygons:
+                for vert_idx, loop_idx in zip(face.vertices, face.loop_indices):
+                    uv_layer.data[loop_idx].uv = blender_UVs[vert_idx]
+            # no idea if this actually works, bing also gave me this
+            normals2 = []
+            for l in bpy_mesh.loops:
+                normals2.append(blender_normals[l.vertex_index])
+            # Set the custom split normals for the mesh
+            bpy_mesh.normals_split_custom_set(normals2)
+            bpy_mesh.use_auto_smooth = True
+
+            ####### BONE JUNK #########
+            if bone_count != -1:
+                # setup bones
+                arm = bpy.data.armatures.new('MyArmature')
+                arm_obj = bpy.data.objects.new('MyArmature', arm)
+                bpy.context.collection.objects.link(arm_obj)
+                mod = obj.modifiers.new('Armature', type='ARMATURE')
+                mod.object = arm_obj
+                
+                bpy.ops.object.select_all(action='DESELECT')
+                bpy.context.view_layer.objects.active = arm_obj
+                bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+                ebs = arm.edit_bones
+
+                # Create the bones
+                bones = []
+                for i in range(0, bone_count):
+                    name = bone_names[i]
+                    pos_data = bone_orientations[i]
+                    bone = ebs.new(name)
+                    bone.head = (pos_data.pos_x, pos_data.pos_y, pos_data.pos_z)
+                    bone.tail = (pos_data.pos_x, pos_data.pos_y, pos_data.pos_z+1.0)
+                    bones.append(bone)
+                # setup parenting
+                for i in range(0, bone_count):
+                    parent = bone_parents[i]
+                    if parent == -1: continue
+                    ebs.active = bones[parent]
+                    bones[i].select = True
+                    bpy.ops.armature.parent_set (type='OFFSET')
+                    bones[i].select = False
+
+
+                # apply vertex weights
+                bpy.ops.object.mode_set (mode='OBJECT')
+                groups = obj.vertex_groups
+                
+                groups_array = []
+                for i in range(0, bone_count):
+                    group = groups.new(name=bone_names[i])
+                    groups_array.append(group)
+
+                for i in range(part.first_vert_index, part.last_vert_index+1):
+                    vert = verts[i]
+                    # bone1
+                    bone1 = vert.bone_indices & 0xff
+                    weight1 = (vert.bone_weights & 0x3ff) / 1023.0
+                    groups_array[bone1].add([i], weight1, 'ADD')
+                    # bone2
+                    bone2 = (vert.bone_indices >> 8) & 0xff
+                    if bone2 != bone1:
+                        weight2 = ((vert.bone_weights >> 10) & 0x3ff) / 1023.0
+                        groups_array[bone2].add([i], weight2, 'ADD')
+                    # bone3
+                    bone3 = (vert.bone_indices >> 16) & 0xff
+                    if bone3 != bone2:
+                        weight3 = (vert.bone_weights >> 20 & 0x1F) / 31.0
+                        groups_array[bone3].add([i], weight3, 'ADD')
+                    # bone4
+                    bone4 = (vert.bone_indices >> 24) & 0xff
+                    if bone4 != bone3:
+                        weight4 = (vert.bone_weights >> 26 & 0x1F) / 31.0
+                        groups_array[bone4].add([i], weight4, 'ADD')
+
+
+def read_static_model(context, filepath):
     print("running read_some_data...")
     f = open(filepath, mode="rb")
     header = read_model_header(f)
@@ -175,56 +286,62 @@ def read_some_data(context, filepath, use_some_setting):
     indices = read_model_indices(f)
     # read objects
     meshes = read_model_mesh(f)
+    # construct
+    construct_meshes(meshes, verts, indices)
 
-    # now we can load all this into blender
-    for mesh in meshes:
-        bpy_mesh = bpy.data.meshes.new("myMesh")
-        obj = bpy.data.objects.new(mesh.name, bpy_mesh)
-        bpy.context.collection.objects.link(obj)
-
-        # gather local verts
-        blender_verts = []
-        blender_UVs = []
-        blender_normals = []
-        for i in range(mesh.first_vert_index, mesh.last_vert_index+1):
-            vert = verts[i]
-            blender_verts.append((vert.x, vert.y, vert.z))
-            blender_UVs.append((vert.uv_x, vert.uv_y))
-
-            yaw = vert.n_y
-            pitch = vert.n_x
-            x = math.cos(yaw)*math.cos(pitch)
-            y = math.sin(yaw)*math.cos(pitch)
-            z = math.sin(pitch)
-            blender_normals.append((x, y, z)) 
-
-        # gather all the indices together
-        blender_indices = []
-        for i in range(0, mesh.triangles_count):
-            index = (i*3)+mesh.indices_offset
-            blender_indices.append((indices[index]-mesh.first_vert_index, indices[index+1]-mesh.first_vert_index, indices[index+2]-mesh.first_vert_index))
-
-        bpy_mesh.from_pydata(blender_verts, [], blender_indices)
-        
-        # no idea what this does, bing gave me this
-        uv_layer = bpy_mesh.uv_layers.new()
-        bpy_mesh.uv_layers.active = uv_layer
-        for face in bpy_mesh.polygons:
-            for vert_idx, loop_idx in zip(face.vertices, face.loop_indices):
-                uv_layer.data[loop_idx].uv = blender_UVs[vert_idx]
-        
-        # no idea if this actually works, bing also gave me this
-        normals2 = []
-        for l in bpy_mesh.loops:
-            normals2.append(blender_normals[l.vertex_index])
-        # Set the custom split normals for the mesh
-        bpy_mesh.normals_split_custom_set(normals2)
-        bpy_mesh.use_auto_smooth = True
+    f.close()
+    return {'FINISHED'}
 
 
+map_model_vertex_size = 32
+class map_model_vertex:
+    x = 0.0     # 0x00, float
+    y = 0.0     # 0x04, float
+    z = 0.0     # 0x08, float
+    n_x = 0.0   # 0x0C, uint
+    n_y = 0.0   # 0x10, uint
+    uv_x = 0.0  # 0x14, float16
+    uv_y = 0.0  # 0x16, float16
+    unk_float = 0.0
+    unk_uint = 0.0
+def read_map_model(context, filepath):
+    print("running read_some_data...")
+    f = open(filepath, mode="rb")
+    header = read_model_header(f)
+
+    vert_something = read_int(f)
+    vert_byte_length = read_int(f)
+    # read verts
+    vert_count = vert_byte_length // map_model_vertex_size
+    verts = []
+    for i in range(0, vert_count):
+        vert = map_model_vertex()
+        vert.x = read_float(f)
+        vert.y = read_float(f)
+        vert.z = read_float(f)
+        vert.n_x = read_norm(f)
+        vert.n_y = read_norm(f)
+        vert.uv_x = read_float16(f)
+        vert.uv_y = read_float16(f)
+        vert.unk_float = read_float(f)
+        vert.unk_uint = read_uint(f)
+        verts.append(vert)
+    
+    # read indices
+    indices = read_model_indices(f)
+
+    # read objects
+    meshes = read_model_mesh(f)
+    # construct
+    construct_meshes(meshes, verts, indices)
+
+    f.close()
     return {'FINISHED'}
 
 class bone_data:
+    pos_x = 0
+    pos_y = 0
+    pos_z = 0
     unk_uint = 0
     float_unk1 = 0
     float_unk2 = 0
@@ -234,12 +351,7 @@ class bone_data:
     float_unk6 = 0 # typically 1.0
     float_unk7 = 0 # typically 1.0
     uint_flags = 0
-    pos_x = 0
-    pos_y = 0
-    pos_z = 0
-
-
-model_vertex_size = 32
+skinned_model_vertex_size = 32
 class bone_vertex:
     x = 0.0     # 0x00, float
     y = 0.0     # 0x04, float
@@ -250,9 +362,7 @@ class bone_vertex:
     uv_y = 0.0  # 0x16, float16
     bone_weights = 0 # 0x18, uint
     bone_indices = 0 # 0x1C, uint
-
-
-def read_some_rigged_data(context, filepath, use_some_setting):
+def read_rigged_model(context, filepath):
     print("running read_some_data...")
     f = open(filepath, mode="rb")
     header = read_model_header(f)
@@ -267,14 +377,14 @@ def read_some_rigged_data(context, filepath, use_some_setting):
     for i in range(0, bone_count):
         bone_parents.append(read_int(f)) # first will be -1 for the base bone
     
-    bone_unknown_index1 = read_uint(f)
-    bone_unknown_index2 = read_uint(f)
-    bone_unknown_index3 = read_uint(f) 
 
     # then read the bone orientation things
     bone_orientations = []
     for i in range(0, bone_count):
         orientation = bone_data()
+        orientation.pos_x = read_float(f)
+        orientation.pos_y = read_float(f)
+        orientation.pos_z = read_float(f)
         orientation.unk_uint = read_uint(f)
         orientation.float_unk1 = read_float(f)
         orientation.float_unk2 = read_float(f)
@@ -284,23 +394,23 @@ def read_some_rigged_data(context, filepath, use_some_setting):
         orientation.float_unk6 = read_float(f)
         orientation.float_unk7 = read_float(f)
         orientation.uint_flags = read_uint(f)
-        orientation.pos_x = read_float(f)
-        orientation.pos_y = read_float(f)
-        orientation.pos_z = read_float(f)
         bone_orientations.append(orientation)
     
-    # then theres a buncha random junk here (5 position floats)
+    # then theres a buncha random junk here (8 position floats)
     unk_pos1 = read_float(f)
     unk_pos2 = read_float(f)
-    unk_pos3 = read_float(f)
+    unk_pos3 = read_float(f) 
     unk_pos4 = read_float(f)
     unk_pos5 = read_float(f)
+    unk_pos6 = read_float(f)
+    unk_pos7 = read_float(f)
+    unk_pos8 = read_float(f)
 
     # then read the vertices
     vert_something = read_int(f)
     vert_byte_length = read_int(f)
     # read verts
-    vert_count = vert_byte_length // model_vertex_size
+    vert_count = vert_byte_length // skinned_model_vertex_size
     verts = []
     for i in range(0, vert_count):
         vert = bone_vertex()
@@ -319,58 +429,9 @@ def read_some_rigged_data(context, filepath, use_some_setting):
     indices = read_model_indices(f)
     # read objects
     meshes = read_model_mesh(f)
-
-    print(len(verts))
-    print(len(indices))
-    print(len(meshes))
-    print(len(meshes[0].parts))
-
     # now we can load all this into blender
-    for mesh in meshes:
-
-        for part in mesh.parts:
-            #part_mat = bpy.data.materials.new(name=part.label)
-            #obj.data.materials.append(part_mat)
-            bpy_mesh = bpy.data.meshes.new("myMesh")
-            obj = bpy.data.objects.new(mesh.name + "_" + part.label, bpy_mesh)
-            bpy.context.collection.objects.link(obj)
-
-            # gather local verts
-            blender_verts = []
-            blender_UVs = []
-            blender_normals = []
-            for i in range(part.first_vert_index, part.last_vert_index+1):
-                vert = verts[i]
-                blender_verts.append((vert.x, vert.y, vert.z))
-                blender_UVs.append((vert.uv_x, vert.uv_y))
-                blender_normals.append((math.cos(vert.n_y)*math.cos(vert.n_x), math.sin(vert.n_y)*math.cos(vert.n_x), math.sin(vert.n_x))) 
-
-            # gather all the indices together
-            blender_indices = []
-            for i in range(0, part.triangles_count):
-                index = (i*3)+part.indices_offset
-                blender_indices.append((indices[index]-part.first_vert_index, indices[index+1]-part.first_vert_index, indices[index+2]-part.first_vert_index))
-
-            bpy_mesh.from_pydata(blender_verts, [], blender_indices)
-            
-            print("exporting mesh")
-
-            #########  EXTRA JUNK ########
-            # no idea what this does, bing gave me this
-            uv_layer = bpy_mesh.uv_layers.new()
-            bpy_mesh.uv_layers.active = uv_layer
-            for face in bpy_mesh.polygons:
-                for vert_idx, loop_idx in zip(face.vertices, face.loop_indices):
-                    uv_layer.data[loop_idx].uv = blender_UVs[vert_idx]
-            # no idea if this actually works, bing also gave me this
-            normals2 = []
-            for l in bpy_mesh.loops:
-                normals2.append(blender_normals[l.vertex_index])
-            # Set the custom split normals for the mesh
-            bpy_mesh.normals_split_custom_set(normals2)
-            bpy_mesh.use_auto_smooth = True
-
-    
+    construct_meshes(meshes, verts, indices, bone_count, bone_names, bone_orientations, bone_parents)
+    f.close()
     return {'FINISHED'}
 
 
@@ -398,26 +459,24 @@ class ImportSomeData(Operator, ImportHelper):
         maxlen=255,  # Max internal buffer length, longer would be clamped.
     )
 
-    # List of operator properties, the attributes will be assigned
-    # to the class instance from the operator settings before calling.
-    use_setting: BoolProperty(
-        name="Example Boolean",
-        description="Example Tooltip",
-        default=True,
-    )
-
     type: EnumProperty(
-        name="Example Enum",
-        description="Choose between two items",
+        name="Import type",
+        description="Choose type of model to import",
         items=(
-            ('OPT_A', "First Option", "Description one"),
-            ('OPT_B', "Second Option", "Description two"),
+            ('OPT_A', "Static model", "Import a static model"),
+            ('OPT_B', "Map model", "Import a map model"),
+            ('OPT_C', "Skinned model", "Import a skinned model"),
         ),
         default='OPT_A',
     )
 
     def execute(self, context):
-        return read_some_rigged_data(context, self.filepath, self.use_setting)
+        if self.type == 'OPT_A':
+            return read_static_model(context, self.filepath)
+        elif self.type == "OPT_B":
+            return read_map_model(context, self.filepath)
+        else:
+            return read_rigged_model(context, self.filepath)
 
 
 # Only needed if you want to add into a dynamic menu.
